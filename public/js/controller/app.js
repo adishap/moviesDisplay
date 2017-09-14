40e36c3a1df3ef96e780bc43a169f1aee8462e52
adishap <porwaladisha@gmail.com>
2017-09-14 10:33:32 +0530

    add controller for movie detail

--- public/js/controller/app.js
+++ public/js/controller/app.js
@@ -1,33 +1,26 @@
-window.moviesApp = angular.module('moviesApp', ['ngRoute']);
+window.moviesApp = angular.module('moviesApp', ['ngRoute'])
+    .constant('constants', {
+        baseURL: 'http://api.cinemalytics.in/v2/',
+        authToken: '9ADE42B2C8DE0A103296C9E6B4200904'
+    });
 
-moviesApp.controller("actorsController", function($scope, $http) {
+moviesApp.controller("actorsController", function($scope, constants, $http) {
     $scope.actors = [];
     $http({
             method: 'GET',
-            url: 'http://api.cinemalytics.in/v2/analytics/MaleActorsByHighestRating'
+            url: constants.baseURL + 'analytics/MaleActorsByHighestRating'
         })
         .success(function(data) {
             $scope.actors = $scope.actors.concat(data);
         });
     $http({
             method: 'GET',
-            url: 'http://api.cinemalytics.in/v2/analytics/FemaleActorsByHighestRating'
+            url: constants.baseURL + 'analytics/FemaleActorsByHighestRating'
         })
         .success(function(data) {
             $scope.actors = $scope.actors.concat(data);
         });
 
-
-    $scope.currActor = null;
-    $scope.getActorById = function(id) {
-        var actors = $scope.actors;
-        for (var i = 0; i < actors.length; i++) {
-            var actor = $scope.actors[i];
-            if (actor.Id == id) {
-                $scope.currActor = actor;
-            }
-        }
-    };
     // A simple back function, that will help us navigate between views
     $scope.back = function() {
         window.history.back();
@@ -36,6 +29,27 @@ moviesApp.controller("actorsController", function($scope, $http) {
         return new Array(parseInt(n));
     };
 });
-moviesApp.controller("actorDetailsController", function($scope, $routeParams) {
-    $scope.getActorById($routeParams.id);
+moviesApp.controller("actorDetailsController", function($scope, $http, constants, $routeParams) {
+    var id = $routeParams.id;
+    var authToken = constants.authToken;
+
+    $http({
+            method: 'GET',
+            url: constants.baseURL + 'actor/' + id + '/movies/?auth_token=' + authToken
+        })
+        .success(function(data) {
+            $scope.movies = data;
+        });
+});
+moviesApp.controller("movieDetailsController", function($scope, $http, constants, $routeParams) {
+    var id = $routeParams.id;
+    var authToken = constants.authToken;
+
+    $http({
+            method: 'GET',
+            url: constants.baseURL + 'movie/id/' + id + '/?auth_token=' + authToken
+        })
+        .success(function(data) {
+            $scope.movie = data;
+        });
 });
